2c9de61e185919be5497edccaac52e803db92eac
Clren07 <clerensiacatherine@gmail.com>
2025-10-03 15:08:01 +0700

    Update guruRoutes.js

--- routes/guruRoutes.js
+++ routes/guruRoutes.js
@@ -2,19 +2,26 @@ const express = require('express');
 const router = express.Router();
 const guruController = require('../controllers/guruController');
 
+function isLoggedIn(req, res, next) {
+    if (!req.session.user) {
+      return res.redirect('/?error=Anda+belum+login');
+    }
+    next();
+  }
+
 // Tampilkan daftar guru
-router.get('/', guruController.showGuru);
+router.get('/', isLoggedIn, guruController.showGuru);
 
 // Tambah guru
-router.post('/add', guruController.addGuru);
+router.post('/add', isLoggedIn, guruController.addGuru);
 
 // Tampilkan halaman edit guru
-router.get('/edit/:id', guruController.showEditGuru);
+router.get('/edit/:id', isLoggedIn, guruController.showEditGuru);
 
 // Update guru
-router.post('/edit/:id', guruController.editGuru);
+router.post('/edit/:id', isLoggedIn, guruController.editGuru);
 
 // Hapus guru
-router.post('/delete/:id', guruController.deleteGuru);
+router.post('/delete/:id', isLoggedIn, guruController.deleteGuru);
 
 module.exports = router;
